795c93ed5ba316e6cabbfea04250086b772286d9
myron <zhainandb@sina.com>
2019-01-05 18:29:57 +0800

    Myron: fix code in time_consumption.

--- g_air/utils/decorator.py
+++ g_air/utils/decorator.py
@@ -6,6 +6,7 @@
 # **********************************************************************************#
 """
 from datetime import datetime
+from functools import wraps
 
 
 def time_consumption(func):
@@ -18,11 +19,12 @@ def time_consumption(func):
     Returns:
         func: decorator function
     """
+    @wraps(func)
     def _decorator(*args, **kwargs):
         start_time = datetime.now()
         result = func(*args, **kwargs)
         end_time = datetime.now()
-        print('time consumption: {}'.format(end_time - start_time))
+        print('[Time consumption] [{}]: {}'.format(func.__name__, end_time - start_time))
         return result
 
     return _decorator
